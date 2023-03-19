572e14afa5f0e0a447eed518e6133f46515a138f
francexavimessi <francexavimessi@gmail.com>
2023-03-19 21:40:40 +0700

    edit

---
+++ my-app/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './css/App.css';
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
+import Signin from './Sigin.js';
+import Profile from './Profile.js';
+import Category from './view/Category.js';
+
+function App() {
+  const token = localStorage.getItem('accessToken');
+
+  // if (!token) {
+  //   return <Signin />
+  // }
+  return (
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/profile">
+    //       <Profile />
+    //     </Route>
+
+    //     <Route path="/abc">
+    //       <Signin />
+    //     </Route>
+    //     <Route path="/category">
+    //       <Category />
+    //     </Route>
+    //     <Route path="/">
+    //       <Category />
+    //     </Route>
+
+    //   </Routes>
+    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Category />} />
+        <Route path="/abc" element={<Signin />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ my-app/src/view/Category.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import swal from 'sweetalert';
+import '../css/Category.css';
+const useStyles = makeStyles((theme) => ({
+//   square: {
+    
+//   },
+//   image: {
+//     backgroundImage: 'url(https://source.unsplash.com/random)',
+//     backgroundSize: 'cover',
+//   },
+//   paper: {
+//     margin: theme.spacing(8, 4),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main,
+//   },
+//   form: {
+//     width: '100%',
+//     marginTop: theme.spacing(1),
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//   },
+}));
+
+
+
+export default function Category() {
+
+
+  return (
+    <div class="body">
+        <div class="square">
+        lkkkkkk
+    </div>
+    </div>
+    
+    
+  );
+}
